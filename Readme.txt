73ee3c83889eabfe74463866c7e52e070ef48470
weidongshan <weidongshan@qq.com>
2015-09-08 11:08:10 +0800

    have jni and hal

--- Readme.txt
+++ Readme.txt
@@ -39,3 +39,37 @@ $ mmm frameworks/base/services
 $ make snod
 $ ./gen-img.sh
 
+
+
+V2:
+(3) JNI: 重新上传
+frameworks/base/services/core/jni/com_android_server_LedService.cpp
+
+(4) HAL: led_hal.h
+         led_hal.c 
+把新文件上传到服务器, 所在目录:
+hardware/libhardware/include/hardware/led_hal.h
+hardware/libhardware/modules/led/led_hal.c
+hardware/libhardware/modules/led/Android.mk
+
+Android.mk内容如下:
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := led.default
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_C_INCLUDES := hardware/libhardware
+LOCAL_SRC_FILES := led_hal.c
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+编译：
+$ mmm frameworks/base/services
+$ mmm hardware/libhardware/modules/led
+$ make snod
+$ ./gen-img.sh
+
+
+
--- com_android_server_LedService.cpp
+++ com_android_server_LedService.cpp
@@ -15,35 +15,50 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <hardware/led_hal.h>
 
 
 namespace android
 {
 
-static jint fd;
+static led_device_t* led_device;
 
 jint ledOpen(JNIEnv *env, jobject cls)
 {
-	fd = open("/dev/leds", O_RDWR);
-	ALOGI("native ledOpen : %d", fd);
-	if (fd >= 0)
-		return 0;
-	else
-		return -1;
+	jint err;
+    hw_module_t* module;
+	hw_device_t* device;
+
+	ALOGI("native ledOpen ...");
+
+	/* 1. hw_get_module */
+    err = hw_get_module("led", (hw_module_t const**)&module);
+    if (err == 0) {
+		/* 2. get device : module->methods->open */
+	    err = module->methods->open(module, NULL, &device);
+	    if (err == 0) {
+			/* 3. call led_open */
+	        led_device = (led_device_t *)device;
+			return led_device->led_open(led_device);
+	    } else {
+	        return -1;
+    	}
+    }
+	
+    return -1;	
 }
 
 void ledClose(JNIEnv *env, jobject cls)
 {
-	ALOGI("native ledClose ...");
-	close(fd);
+	//ALOGI("native ledClose ...");
+	//close(fd);
 }
 
 
 jint ledCtrl(JNIEnv *env, jobject cls, jint which, jint status)
 {
-	int ret = ioctl(fd, status, which);
-	ALOGI("native ledCtrl : %d, %d, %d", which, status, ret);
-	return ret;
+	ALOGI("native ledCtrl %d, %d", which, status);
+	return led_device->led_ctrl(led_device, which, status);
 }
 
 
